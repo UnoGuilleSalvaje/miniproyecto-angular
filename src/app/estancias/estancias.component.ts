c5e7026c8e72f418233575b195c565a266f387eb
UnoGuilleSalvaje <unoguillel@gmail.com>
2024-04-16 17:02:07 -0600

    estancia

--- src/app/estancias/estancias.component.ts
+++ src/app/estancias/estancias.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Estancias } from '../interfaces/estancias';
+import { EstanciasService } from '../services/estancias.service';
 
 @Component({
   selector: 'app-estancias',
@@ -8,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './estancias.component.css'
 })
 export class EstanciasComponent {
+  array:Estancias [] = [];
 
+  constructor(public estanciasService: EstanciasService) {}
+
+  ngOnInit() {
+    console.log("En este instante el componente ha cargado");
+    this.recuperarDatos();
+  }
+
+  recuperarDatos(): void {
+    console.log("Entré");
+
+    this.estanciasService.retornar().subscribe({
+      next: this.successRequest.bind(this),
+      error: (err) => {console.log(err)}
+    });
+  }
+
+  successRequest(data:any):void {
+    console.log("data", data);
+    this.array = data.estancias;
+    console.log("array", this.array);
+  }
 }
---
+++ src/app/interfaces/estancias.ts
@@ -0,0 +1,10 @@
+export interface Estancias {
+    id: number;
+    nombre: string;
+    propietario: string;
+    telefono: string;
+    email: string;
+    precio: number;
+    direccion: string;
+    imagen: string;
+}
