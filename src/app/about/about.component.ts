9ea91283b81a7c84e583661c682e1cd31d2496ff
Arely Zuleika <96224990+Moonrise777@users.noreply.github.com>
2024-05-01 21:57:05 -0600

    About page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, NgStyle],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
