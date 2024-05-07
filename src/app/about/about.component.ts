9eb9d9bba3cb48437e2c230512046423c497ffbc
Arely Zuleika <96224990+Moonrise777@users.noreply.github.com>
2024-05-06 21:52:40 -0600

    material icons link

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { NgStyle } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, NgStyle],
+  imports: [FormsModule, MatCardModule, MatIconModule, NgStyle],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
