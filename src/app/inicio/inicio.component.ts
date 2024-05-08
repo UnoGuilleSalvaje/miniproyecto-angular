fbb3a5bfef7b836d5781fc48b4a146c660b6d5b6
Amerix <151253292+Amerix@users.noreply.github.com>
2024-05-07 19:59:59 -0600

    Update inicio.component.ts

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { NgStyle } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DomseguroPipe } from '../domseguro.pipe';
+
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule ({
+  imports: [
+    MatSlideToggleModule,
+  ]
+})
+class AppModule {}
+
+export class MiComponenteComponent {
+  isMagic: boolean = false;
+
+  toggleMagic() {
+    this.isMagic = !this.isMagic;
+  }
+}
+
 
 @Component({
   selector: 'app-inicio',
@@ -15,6 +34,12 @@ import { DomseguroPipe } from '../domseguro.pipe';
 })
 export class InicioComponent {
   userEmail: string = '';
+
+  ultimasReservaciones = [
+    { id: 1, tipo: 'Reservación', fecha: '2024-05-01', cliente: 'Juan', habitacion: '101' },
+    { id: 2, tipo: 'Oferta', fecha: '2024-05-02', producto: 'Descuento en Habitaciones' },
+    { id: 3, tipo: 'Reservación', fecha: '2024-05-03', cliente: 'María', habitacion: '102' }
+  ];
 
   updateEmail() {
     console.log('Email actualizado:', this.userEmail);
