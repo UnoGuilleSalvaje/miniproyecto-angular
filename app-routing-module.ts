89296d2029183e479dea0120b8aa0aa211937cdf
AxelParedes <124729598+AxelParedes@users.noreply.github.com>
2024-05-07 19:44:46 -0600

    Add files via upload

---
+++ app-routing-module.ts
@@ -0,0 +1,6 @@
+import { FelicitarComponent } from "./inicio/felicitar/felicitar.component";
+
+const routes = [
+    { path: 'felicitar', component: FelicitarComponent },
+    // Otras rutas
+];
---
+++ app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ReporteComponent } from './reporte/reporte.component';
+import { EstanciasComponent } from './estancias/estancias.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { UnaEstanciaComponent } from './una-estancia/una-estancia.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { FelicitarComponent } from './inicio/felicitar/felicitar.component';
+
+export const routes: Routes = [
+    {path: 'inicio', component: InicioComponent},
+    {path: 'formulario', component: FormularioComponent},
+    {path: 'reporte', component: ReporteComponent},
+    {path: 'estancias', component: EstanciasComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'search', component: SearchComponent},
+    {path: 'estancia/:id', component: UnaEstanciaComponent},
+    {path: 'buscador/:nombree', component: SearchComponent},
+    {path: 'felicitar', component: FelicitarComponent},
+    {path: '**', redirectTo: 'inicio'}
+];
